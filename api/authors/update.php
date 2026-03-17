17e470198a5e1cb87d601cb645b2998f864b63f3
Cole Brown <c_brown23@mail.fhsu.edu>
2026-03-17 04:55:45 -0500

    fixed minor syntax error on cors headers and error handling for update auth and cat

--- api/authors/update.php
+++ api/authors/update.php
@@ -1,7 +1,7 @@
 <?php
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
-    header('Access-Control-Allow-Mehtods: PUT');
+    header('Access-Control-Allow-Methods: PUT');
     header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
     include_once '../../config/Database.php';
@@ -15,16 +15,21 @@
 
     $data = json_decode(file_get_contents("php://input"));
 
-    $author->id = $data->id;
-    $author->author = $data->author;
+    $author->id = isset($data->id) ? $data->id : NULL;
+    $author->author = isset($data->author) ? $data->author : NULL;
 
-    if($author->update()){
-        echo json_encode(
-            array('message' => 'Author Updated')
-        );
+    if(($author->id != NULL) && ($author->author != NULL)){
+        if($author->update()){
+                $author_arr = array(
+                    'id' => $author->id,
+                    'author' => $author->author
+                );
+            
+                print_r(json_encode($author_arr));
+        }
     }else{
         echo json_encode(
-            array('message' => 'Author Not Updated')
+            array('message' => 'Missing Required Parameters')
         );
     }
 ?>
--- api/categories/update.php
+++ api/categories/update.php
@@ -1,7 +1,7 @@
 <?php
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
-    header('Access-Control-Allow-Mehtods: PUT');
+    header('Access-Control-Allow-Methods: PUT');
     header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
     include_once '../../config/Database.php';
@@ -16,15 +16,16 @@
     $data = json_decode(file_get_contents("php://input"));
 
     $category->id = isset($data->id) ? $data->id : NULL;
-    $category->id = isset($data->category) ? $data->category : NULL;
+    $category->category = isset($data->category) ? $data->category : NULL;
 
-    if(($category->id != NULL) && ($category->id != NULL)){
+    if(($category->id != NULL) && ($category->category != NULL)){
         if($category->update()){
-            echo json_encode(
-                array(
+                $category_arr = array(
                     'id' => $category->id,
-                    'author' => $category->category
-                ));
+                    'category' => $category->category
+                );
+            
+                print_r(json_encode($category_arr));
         }
     }else{
         echo json_encode(
